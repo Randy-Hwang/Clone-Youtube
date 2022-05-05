1f62b8932051b70e9a5b063f104cbe62526122f0
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-05-05 16:29:01 +0900

    add server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+// express를 express라는 이름으로 import
+import express from "express";
+
+const PORT = 4000;
+
+// express와 관련된 코드는 express application 을 선언한 이후에 적어야 합니다.
+const app = express();
+// application에게 get request에 응답하는 방법 등을 알려줘야 합니다.
+
+const handleHome = (req, res) => {
+  return res.send("Hello World!");
+};
+// app.get("/") -> home router(/)에 요청이 들어왔을 때 어떤 반응을 보여줘야 할지에 대해서.
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`GO to Our Server! -> http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
