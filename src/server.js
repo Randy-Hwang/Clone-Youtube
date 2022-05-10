f10f398734fc022bf368b575db457001bf5e5a02
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-05-10 12:15:21 +0900

    server test

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // express를 express라는 이름으로 import
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
@@ -7,9 +8,16 @@ const PORT = 4000;
 const app = express();
 // application에게 get request에 응답하는 방법 등을 알려줘야 합니다.
 
+const logger = morgan("dev");
+
 const handleHome = (req, res) => {
-  return res.send("Hello World!");
+  return res.send("I Love Express!");
 };
+
+//middleware를 어떤 url에서나 쓸 수 있도록 하는 함수. 모든 라우트(route)에서 이 middleware 함수를 사용하게 됨
+// morgan -> 정보를 조금 더 정교하게 줌.
+app.use(logger);
+
 // app.get("/") -> home router(/)에 요청이 들어왔을 때 어떤 반응을 보여줘야 할지에 대해서.
 app.get("/", handleHome);
 
